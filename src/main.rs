a4a78fb8c64784a85d61b8ea331c7ef85f529f0c
Bikossor <18504590+Bikossor@users.noreply.github.com>
2026-08-19 21:53:21 +0200

    feat: set up axum and tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app_router = Router::new().fallback(hello_world);
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
+
+    axum::serve(listener, app_router).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn hello_world() -> &'static str {
+    "Hello, world!"
 }
