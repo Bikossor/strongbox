bbca6f2cf43cc4a1f2ca5ad6f9258bf9b55c8599
Bikossor <18504590+Bikossor@users.noreply.github.com>
2026-08-22 11:04:15 +0200

    feat: expand and prepare the app_router

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use axum::Router;
+use axum::{
+    Router,
+    routing::{get, post},
+};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app_router = Router::new().fallback(hello_world);
+    let public_router = Router::new()
+        .route("/health", get(health_check))
+        .route("/register", post(register_user))
+        .route("/login", post(login_user));
+    let protected_router = Router::new().route("/logout", post(logout));
+
+    let api_router = Router::new().merge(public_router).merge(protected_router);
+
+    let app_router = Router::new()
+        .nest("/api/v1", api_router)
+        .fallback(hello_world);
+
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     axum::serve(listener, app_router).await.unwrap();
@@ -12,4 +26,20 @@ async fn main() {
 // basic handler that responds with a static string
 async fn hello_world() -> &'static str {
     "Hello, world!"
+}
+
+async fn health_check() {
+    todo!()
+}
+
+async fn register_user() {
+    todo!()
+}
+
+async fn login_user() {
+    todo!()
+}
+
+async fn logout() {
+    todo!()
 }
