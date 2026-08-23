dd37414ee21a5601b2c392c89c70747ca3bea827
Bikossor <18504590+Bikossor@users.noreply.github.com>
2026-08-23 23:56:16 +0200

    feat: add ApiError

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
 use axum::{
-    Router,
+    Json, Router,
     extract::{Request, State},
+    http::StatusCode,
     middleware::{self, Next},
-    response::Response,
+    response::{IntoResponse, Response},
     routing::{get, get_service, post},
 };
+use serde_json::json;
 use tokio::net::TcpListener;
 use tower_http::services::{ServeDir, ServeFile};
 
 #[derive(Clone)]
 struct AppState {}
+
+enum ApiError {
+    Conflict,
+    InternalServerError,
+    NotFound,
+    Unauthorized,
+}
+
+impl IntoResponse for ApiError {
+    fn into_response(self) -> Response {
+        let (status, message) = match self {
+            ApiError::Conflict => (StatusCode::CONFLICT, "Conflict".to_string()),
+            ApiError::InternalServerError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Internal server error".to_string(),
+            ),
+            ApiError::NotFound => (StatusCode::NOT_FOUND, "Resource not found".to_string()),
+            ApiError::Unauthorized => (StatusCode::UNAUTHORIZED, "Unauthorized".to_string()),
+        };
+
+        let body = Json(json!({ "message": message }));
+        (status, body).into_response()
+    }
+}
 
 #[tokio::main]
 async fn main() {
